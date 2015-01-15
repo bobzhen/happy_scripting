1ff75895ad2acac5ea3a9fce820078e07aa84821
Hongbo <hongbo.zhen@gmail.com>
2015-01-15 22:35:59 +0800

    Create fetch_images.rb

---
+++ fetch_images.rb
@@ -0,0 +1,15 @@
+require 'open-uri'
+require 'uri'
+
+# input arguments by using ARGV[N]
+arg001 = ARGV[0]
+# '../fetch_image_test.html'
+html = open(arg001).read
+image_array = URI.extract(html).select{ |l| l[/\.(?:gif|png|jpg)\b/]}
+puts image_array.length
+today = `date`
+image_array.each do |i|
+	tmp = 'curl -O "' + i + '"'
+	puts tmp.to_s
+	system tmp
+end
